ea120d2d543c74efb6e04d01798c67200a6525a2
Brightheadit1 <ayush@brightheadit.com>
2025-10-14 17:55:42 +0530

    tailwind.config.ts issue

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,9 @@ const config: Config = {
   content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      scrollbar: ['rounded'],
+      scrollbar: {
+        rounded: true, 
+      },
     },
   },
   plugins: [
